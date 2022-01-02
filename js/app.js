5f1afa6f02e64b28d54bd55b7d087222388c6e27
YA <YA-androidapp@users.noreply.github.com>
2022-01-02 17:18:22 +0900

    Update UI

--- js/app.js
+++ js/app.js
@@ -1,8 +1,24 @@
 // Copyright (c) 2022 YA-androidapp(https://github.com/YA-androidapp) All rights reserved.
 window.addEventListener('DOMContentLoaded', (event) => {
+    initializeBootstrap();
+    initializeUI();
+    checkTouchFeature();
+});
+
+const checkTouchFeature = () => {
+    if (navigator.maxTouchPoints > 0) {
+        hideOverlay();
+    }
+}
+
+const hideOverlay = () => {
+    const overlayElem = document.getElementById("overlay");
+    overlayElem.style.display = "none";
+}
+
+const initializeBootstrap = () => {
     // Alerts
     let alertList = document.querySelectorAll('.alert');
-    console.log("alertList", alertList);
     alertList.forEach(function (alert) {
         new bootstrap.Alert(alert);
     });
@@ -25,6 +41,35 @@ window.addEventListener('DOMContentLoaded', (event) => {
         })
     });
 
+    const infoAlertClose = document.getElementById("infoAlertClose");
+    infoAlertClose.addEventListener("click", function () {
+        let alert = document.getElementById("infoAlert");
+        alert.classList.remove("show");
+        alert.style.display = "none";
+    });
+
+    const copiedAlertClose = document.getElementById("copied-alert-close");
+    copiedAlertClose.addEventListener("click", function () {
+        let alert = document.getElementById("copied-alert");
+        alert.classList.remove("show");
+        alert.style.display = "none";
+    });
+
+    const warningAlertClose = document.getElementById("warningAlertClose");
+    warningAlertClose.addEventListener("click", function () {
+        let alert = document.getElementById("warningAlert");
+        alert.classList.remove("show");
+        alert.style.display = "none";
+    });
+
+    const dangerAlertClose = document.getElementById("dangerAlertClose");
+    dangerAlertClose.addEventListener("click", function () {
+        let alert = document.getElementById("dangerAlert");
+        alert.classList.remove("show");
+        alert.style.display = "none";
+    });
+
+
     // Popovers
     var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
     var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
@@ -44,4 +89,27 @@ window.addEventListener('DOMContentLoaded', (event) => {
     // Tooltips
     var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
     var tooltipList = tooltipTriggerList.map((tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl));
-});
+}
+
+const initializeUI = () => {
+    document.getElementById('copyMenu').addEventListener('click', () => {
+        const img = document.getElementById('mainImg');
+        const canvas = document.createElement('canvas');
+        canvas.width = img.naturalWidth;
+        canvas.height = img.naturalHeight;
+        const ctx = canvas.getContext('2d');
+        ctx.drawImage(img, 0, 0);
+
+        canvas.toBlob(async (blob) => {
+            const item = new ClipboardItem({
+                'image/png': blob
+            });
+            await navigator.clipboard.write([item]);
+
+            let alert = document.getElementById("copied-alert");
+            alert.classList.remove("show");
+            alert.classList.add("show");
+            alert.style.display = "flex";
+        });
+    });
+}
